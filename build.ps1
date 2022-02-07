c2edd731c3b74cb8e5bfb7b842fffe1b0757af86
Trym Lund Flogard <trym@flogard.no>
2022-02-07 16:30:46 +0100

    set nbgv in build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,16 @@
+ $version = "1.0.0"
+ if ($(where.exe nbgv)) {
+    Write-Host "building:"
+    nbgv get-version
+    nbgv cloud -p . -s GitHubActions
+    $version = $(nbgv get-version -v NuGetPackageVersion)
+}
+else {
+    Write-Host "nbgv not found. reverting to $version."
+}
+ 
  msbuild TrayIcon /p:Platform=x64 /p:Configuration=Release
  msbuild TrayIcon /p:Platform=win32 /p:Configuration=Release
  dotnet build TrayIconProjections -c:Release
  
- nuget pack nuget\TrayIcon.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version 1.0.0
+ nuget pack nuget\TrayIcon.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version $version
