2df24ab7870574cf65fadebeb952b60f4ae9d8c5
Trym Lund Flogard <trym@flogard.no>
2023-01-25 19:21:55 +0100

    add VCRUNTIME140_APP support

--- build.ps1
+++ build.ps1
@@ -10,8 +10,13 @@ else {
 }
  
 msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release
-dotnet build SimpleTrayIconProjections -c:Release /p:Platform=AnyCPU
 msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release
+
 dotnet build SimpleTrayIconProjections -c:Release /p:Platform=AnyCPU
- 
- nuget pack nuget\SimpleTrayIcon.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version $version -properties  commit=$(git rev-parse HEAD)
+
+nuget pack nuget\SimpleTrayIcon.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version $version -properties  commit=$(git rev-parse HEAD)
+
+msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release /p:TrayIconOneCore=true
+msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release /p:TrayIconOneCore=true
+
+nuget pack nuget\SimpleTrayIcon.onecore.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version $version -properties  commit=$(git rev-parse HEAD)
