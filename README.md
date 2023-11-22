639660aedef1f376fcb14c8a13a6446f6afa70d2
Trym Lund Flogard <trym@flogard.no>
2023-11-22 22:55:31 +0100

    Static link support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TrayIconBindings
+# SimpleTrayIcon
 A fast, lightweight, object oriented wrapper for the [Shell_NotifyIcon](https://learn.microsoft.com/windows/win32/api/shellapi/nf-shellapi-shell_notifyiconw) API with no framework dependencies.
 
 [![NuGet version (SimpleTrayIcon)](https://img.shields.io/nuget/v/SimpleTrayIcon.svg?style=flat-square)](https://www.nuget.org/packages/SimpleTrayIcon/)
@@ -15,3 +15,27 @@ menu.Items.Add(new TrayMenuItem { Content = $"some content", IsChecked = true })
 item.Click += (s, e) => ((TrayMenuItem)s).IsChecked = !((TrayMenuItem)s).IsChecked; // Attach an event
 NativeMethods.RunLoop(); // Runs the message pump. Winforms, WPF, etc., will do this for you.
 ```
+
+## NativeAOT Linking Options
+
+SimpleTrayIcon is fully AOT compatible.
+
+You can build with static linking to a static library or dynamic library (with DLL).
+
+To enable static linking to the SimpleTrayIcon DLL:
+```xml
+<PropertyGroup>
+    <SimpleTrayIconStaticLinking>true</SimpleTrayIconStaticLinking>
+</PropertyGroup>
+```
+
+To enable static linking to the SimpleTrayIcon static library:
+```xml
+<PropertyGroup>
+    <SimpleTrayIconStaticLinking>true</SimpleTrayIconStaticLinking>
+    <UseSimpleTrayIconStaticLib>true</UseSimpleTrayIconStaticLib>
+</PropertyGroup>
+```
+
+## OneCore
+There is a OneCore SKU of the package `SimpleTrayIcon.onecore` which links to OneCoreUAP and VCRUNTIME140_APP. It is indeed *not* OneCoreUAP compatible, but is compatible with any full-trust UWP apps. This package does not support static linking.
--- build.ps1
+++ build.ps1
@@ -9,16 +9,20 @@ else {
     Write-Host "nbgv not found. reverting to $version."
 }
  
-msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release
-msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release
-msbuild SimpleTrayIcon /p:Platform=ARM64 /p:Configuration=Release
+msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release /p:ConfigurationType=DynamicLibrary /m
+msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release /p:ConfigurationType=DynamicLibrary /m
+msbuild SimpleTrayIcon /p:Platform=ARM64 /p:Configuration=Release /p:ConfigurationType=DynamicLibrary /m
+ 
+msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release /p:ConfigurationType=StaticLibrary /m
+msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release /p:ConfigurationType=StaticLibrary /m
+msbuild SimpleTrayIcon /p:Platform=ARM64 /p:Configuration=Release /p:ConfigurationType=StaticLibrary /m
 
 dotnet build SimpleTrayIconProjections -c:Release /p:Platform=AnyCPU
 
 nuget pack nuget\SimpleTrayIcon.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version $version -properties  commit=$(git rev-parse HEAD)
 
-msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release /p:TrayIconOneCore=true
-msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release /p:TrayIconOneCore=true
-msbuild SimpleTrayIcon /p:Platform=ARM64 /p:Configuration=Release /p:TrayIconOneCore=true
+msbuild SimpleTrayIcon /p:Platform=x64 /p:Configuration=Release /p:TrayIconOneCore=true /p:ConfigurationType=DynamicLibrary /m
+msbuild SimpleTrayIcon /p:Platform=Win32 /p:Configuration=Release /p:TrayIconOneCore=true /p:ConfigurationType=DynamicLibrary /m
+msbuild SimpleTrayIcon /p:Platform=ARM64 /p:Configuration=Release /p:TrayIconOneCore=true /p:ConfigurationType=DynamicLibrary /m
 
 nuget pack nuget\SimpleTrayIcon.onecore.nuspec -nopackageanalysis -basepath . -outputdirectory .\bin -version $version -properties  commit=$(git rev-parse HEAD)
